95bd066f8b89eef01850607d59cb2bb9a8aa6830
nath-techie <64378408+nath-techie@users.noreply.github.com>
2022-06-12 19:35:39 +0530

    first major changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import sabari2 from './sabari2.png';
+import capture2 from './Capture2.JPG';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="App-header">
+        <img src={sabari2} className="App-logo" alt="logo" />
+        <img src={capture2} className="white"/>
+
+
+</div>
     </div>
   );
 }
